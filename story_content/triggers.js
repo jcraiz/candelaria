b716ad3130bd23062e0140e864f2972697dc9086
jcraiz <63567373+jcraiz@users.noreply.github.com>
2025-01-26 18:27:42 -0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,24 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "69rU60djmR3":
+        Script1();
+        break;
+      case "65hYMiKMlmv":
+        Script2();
+        break;
+      case "6cAy61GmnQ6":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
